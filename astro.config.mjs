dca24779462153f456cc532b8edb846183a2d8d6
紫荆 <8132063+zijingfanhuha@user.noreply.gitee.com>
2023-03-24 10:51:16 +0800

    解决git静态资源加载问题1

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-  site: 'https://ZiJingFanHua.github.io/zijingWeb',
-  base: '',
+  site: 'https://ZiJingFanHua.github.io',
+  base: '/zijingWeb',
 });
