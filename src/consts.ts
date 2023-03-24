bf3f95992c5380297e986bac35f16cc93e48e31e
紫荆 <8132063+zijingfanhuha@user.noreply.gitee.com>
2023-03-24 21:48:49 +0800

    修改了链接图标

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+import { link } from "fs";
+
 export const SITE = {
 	title: 'ZiJingBlog',
 	description: 'Your website description.',
@@ -41,6 +43,8 @@ export const SIDEBAR: Sidebar = {
 			{ text: 'Page 2', link: 'docs/en/page-2' },
 			{ text: 'Page 3', link: 'docs/en/page-3' },
 		],
-		'2023': [{ text: 'Page 4', link: 'docs/en/page-4' }],
+		'2023': [{ text: 'Page 4', link: 'docs/en/page-4' },
+		{ text: "Bloom box", link: 'docs/en/bloom-box' }],
+		'框架配置': [{ text: 'Astro快速搭建博客', link: 'docs/en/astro-fast-building' }],
 	},
 };
